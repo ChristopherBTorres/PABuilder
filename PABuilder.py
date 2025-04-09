a143c95fb6364f2ad119aec3d02a6501a870fdee
unknown <ChristopherB.Torres@outlook.com>
2025-04-09 14:09:31 -0500

    Second Iteration: Implemented folder creation for pages of web application based on user input.

--- PABuilder.py
+++ PABuilder.py
@@ -1,22 +1,24 @@
 '''
 Author: Christopher Torres
 Github: https://github.com/ChristopherBTorres
-Version 0.0.1 - Inprogress
+Version 0.0.2 - In-progress
 
 Requires: Python, ng, and npm.
 
 Summary:
 This is program sets out to simplify the setup of a new front-end web application by generating the project, and folder structure based on user input.
 Currently only supports React and Angular.
+
+***Version 0.0.2 Notes - 
+Current build has external script calls commented out for faster testing. Next build will focus on better code readability and organization.
 '''
 #Imports
-import sys, subprocess, re
+import sys, os, subprocess
 
 #Runs external scripts to setup project folder.
-def runScript(command, projectName):
+def runScript(command, formatedProjectName):
     #Generate Command
-    projectName = re.sub(r'\s+', '_', projectName)
-    commandline = command + projectName
+    commandline = command + formatedProjectName
     print(commandline)
     
     #Call Command and wait for result.
@@ -24,8 +26,14 @@ def runScript(command, projectName):
     return 0 #process.returncode
 
 #Introduction
+'''
+Initial prompts need improvement. Creating recursive function in conjunction with better input verification will improve usability and readability.
+'''
 print('Welcome to the Python App Builder.\nThis program is designed to streamline web application setup. Currently only Vite React and Angular are supported.')
 projectName = input('What is your projects name?\n')
+if projectName == "":
+    projectName = input('What is your projects name?\n')
+formatedProjectName = projectName.translate({ord(c): "_" for c in "!@#$%^&*()[]{};:,./<>?\\|`~-=_+"}).lower()
 process = None
 
 while True:
@@ -34,9 +42,9 @@ while True:
         frontend = frontend.lower()
 
         if  frontend == 'angular':
-            process = runScript('ng new ', projectName)
+            process = runScript('ng new ', formatedProjectName)
         elif  frontend == 'react':
-            process = runScript('npm create vite@latest ', projectName)
+            process = runScript('npm create vite@latest ', formatedProjectName)
         else:
             print("Option invalid.\n")
             continue
@@ -47,14 +55,25 @@ while True:
 if process is not None:
     print("Formating Project File...")
     #Call function that creates folder structure based on selected front-end: generateProjectFolderStructure(frontend)
-    pageCount = input("How many pages will this web application need?\n")
-    for i in range(int(pageCount)):
-        #Get page name from user
+    pageCount = int(input("How many pages will this web application need?\n"))
+    i = 0
+    while i < pageCount: #End loop when page count is met.
+        #Get pageName
         pageName = input('Enter a page name: ')
-        pageName = re.sub(r'\s+', '_', pageName.lower())
-        print(pageName)
-        #Generate pages based on the number given by the user
-        print("Generating folder {i}.")
+        formatedPageName = pageName.translate({ord(c): "_" for c in "!@#$%^&*()[]{};:,./<>?\\|`~-=_+"}).lower()
+        
+        #Generate path
+        newpath = os.path.join(formatedProjectName, 'pages', formatedPageName)
+
+        #Try to create folders.
+        try:
+            os.makedirs(newpath)
+            print("Folder %s generated." % pageName)
+            #On success increment loop.
+            i += 1
+        except FileExistsError: #If fail reiterate loop at current i value.
+            print("Folder %s already exists." % pageName)
+            continue
 
 #Program Concludes
 print("Closing PABuilder.")
